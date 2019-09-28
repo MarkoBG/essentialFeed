8cdf48df6ba71b922cc655482ceb34ee538730af
Marko Tribl <triblmarko@gmail.com>
2019-09-28 15:13:41 +0200

    Create the Feed Loader protocol boundary

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Marko Tribl on 9/28/19.
+//  Copyright © 2019 Marko Tribl. All rights reserved.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
